c01d108af0f0fc9cb11a7a6c02a85adb1c814567
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-07 14:45:47 +0700

    update desc

---
+++ scripts/constructor-function.js
@@ -0,0 +1,7 @@
+/*
+Constructor Function
+- Sebelum EcmaScript versi 6 pada saat pembuatan class menggunakan function
+- Untuk membuat class di JavaScript yang lama kita membutuhkan function 
+  dan function tsb dinamakan Cunstructor Function
+*/
+
---
+++ scripts/object-class.js
@@ -0,0 +1,16 @@
+/*
+- Membuat object menggunakan tipe data object  akan membuat object tsb selalu unik
+- Dalam OOP kita membuat class sebagai kerangka/cetakan, 
+  jadi bisa membuat object dengan karakter yang sama berkali-kali tanpa deklarasi object berkali-kali
+- Sebelum EcmaScript versi 6 pada saat pembuatan class menggunakan function
+- 
+*/
+
+const class_student = {
+    name:"Rizki"
+
+}
+
+const class_employee = {
+    name:"Asmoro"
+}
