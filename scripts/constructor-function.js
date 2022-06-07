3b982311c448a3aa3e59b626352c8b2c1e0f672d
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-07 15:14:19 +0700

    property constructor function

--- scripts/constructor-function.js
+++ scripts/constructor-function.js
@@ -5,10 +5,29 @@ Constructor Function
   dan function tsb dinamakan Cunstructor Function
 
 */
-
+{
 function Person(){ //best practice dalam membuat function mengunakan uppercase pada huruf pertama
 
 }
 //membuat object harus menggunakan kata kunci new
 const rizki = new Person();
 const asmoro = new Person();
+}
+
+/* untuk menambahkan property di dalam semua object yang dibuat dari Constructor Function
+bisa menggunakan kata kunci 'this' lalu diikuti dengan nama property nya
+*/
+function Person(){
+    this.firstName="";
+    this.lastName="";
+}
+const rizki = new Person();
+rizki.firstName = "RIZKI";
+rizki.lastName = "ASMORO";
+
+const alkha = new Person();
+alkha.firstName = "ALKHALEENA";
+alkha.lastName = "MAEZURRA";
+
+console.log(rizki);
+console.log(alkha);
