fdbf5a9bb37384361f5bc8cb23027dc6c9a73fd3
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-07 14:55:22 +0700

    constructor function

--- scripts/constructor-function.js
+++ scripts/constructor-function.js
@@ -3,5 +3,12 @@ Constructor Function
 - Sebelum EcmaScript versi 6 pada saat pembuatan class menggunakan function
 - Untuk membuat class di JavaScript yang lama kita membutuhkan function 
   dan function tsb dinamakan Cunstructor Function
+
 */
 
+function Person(){ //best practice dalam membuat function mengunakan uppercase pada huruf pertama
+
+}
+//membuat object harus menggunakan kata kunci new
+const rizki = new Person();
+const asmoro = new Person();
