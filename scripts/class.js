7f2fcd69dcc22148710691df13313c9cf6efa185
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-14 14:42:07 +0700

    constructor class

--- scripts/class.js
+++ scripts/class.js
@@ -1,13 +1,22 @@
-{
+
 class Student {//membuat class
 
 }
 const jaka = new Student(); //membuat object jaka
 console.info(jaka);
-}
+
 
 /*
 Constructor di class
 - Constructor digunakan jika ingin menambahkan parameter pada class
 */
+ class Person {
+    constructor(name){
+        console.info(`Hallo object ${name} ini Constructor di class Person`)
+    }
+ }
+ const x = new Person("X"); // object x
+ console.info(x);
 
+ const y = new Person("Y");
+ console.info(y);
