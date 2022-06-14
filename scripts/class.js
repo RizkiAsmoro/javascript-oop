4139cad45153e2482cde7bf93b0708bd821b8bd2
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-14 14:35:15 +0700

    class

---
+++ scripts/class.js
@@ -0,0 +1,13 @@
+{
+class Student {//membuat class
+
+}
+const jaka = new Student(); //membuat object jaka
+console.info(jaka);
+}
+
+/*
+Constructor di class
+- Constructor digunakan jika ingin menambahkan parameter pada class
+*/
+
