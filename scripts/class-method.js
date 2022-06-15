735904bc149360da6c8f852cf354774389d7dd28
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-15 11:53:57 +0700

    update class method

--- scripts/class-method.js
+++ scripts/class-method.js
@@ -1,12 +1,12 @@
 /*
 Method in Class :
-- Saat menambahkan method pada class maka direkomendasikan di dalam prototype-nya bukan pada object-nya
+- Saat menambahkan method pada class maka direkomendasikan di dalam prototype-nya bukan pada instance object-nya
 */
 
 class Student {
     constructor(name){
         this.nama = name;
-        //jika menambahkan method seperti dibawah ini maka method-nya akan menempel pada object-nya bukan pada prototype-nya
+        //jika menambahkan method seperti dibawah ini maka method-nya akan menempel pada instance object-nya bukan pada prototype-nya
         //dan hal tsb tidak direkomendasikan
         /*this.sayHi = function(name){
             console.info(`Hi ${name}, my name is ${this.nama}`);
@@ -14,9 +14,13 @@ class Student {
     }
     //menambahkan method pada prototype-nya dan ini yang direkomendasikan
     sayHi(name){
-        console.info(`Hi ${name}, my name is ${this.nama}`);
+        console.info(`Hi Mr ${name}, my name is ${this.nama}`);
   }
 }
 const s1 = new Student("Jaka");
 console.info(s1);
 s1.sayHi("James")
+
+const s2 = new Student("Jaka");
+console.info(s2);
+s2.sayHi("Bean")
