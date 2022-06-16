d8ba90b5a041ba7e3e62b71bd8af8411dfe558d4
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-16 11:17:27 +0700

    suepr method

---
+++ scripts/super-method.js
@@ -0,0 +1,22 @@
+/*
+Super Method :
+- Kata kunci super dapat dipakai juga untuk mengakses method parennya dan constructor parent nya
+- caranya ketik super titik nama function-nya
+- super sebenarnya adalah reference ke parent prototype-nya, mirip seperti __proto__
+*/
+
+class highSchool{
+    Teacher(){
+        console.info(`This is method teacher highSchool`)
+    }
+}
+
+class middleSchool extends highSchool{
+    Student(){
+        super.Teacher();
+        console.info(`This is method Student middleSchool`)
+    }
+}
+
+const mSchool = new middleSchool();
+mSchool.Student();
