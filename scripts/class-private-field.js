fa3b40cfb786dd7beeb76f9b4087f93b3e1ee4d7
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-17 14:24:21 +0700

    class:private field

---
+++ scripts/class-private-field.js
@@ -0,0 +1,30 @@
+/*
+Class : Private Field :
+- Secara default saat menambahkan field maka field tsb bisa diakses dimanapun/public
+- Jika ingin private / hanya bisa diakes di dalam class tsb saja maka kata kuncinya adalah # sebelum nama fiel-nya
+*/
+
+class Counter {
+    #counter = 0;
+
+    increment(){
+        this.#counter++;
+    }
+    decrement(){
+        this.#counter--;
+    }
+    get(){
+        return this.#counter;
+    }
+}
+
+const xcounter = new Counter();
+
+//naikan nilai 3 kali
+xcounter.increment();
+xcounter.increment();
+xcounter.increment();
+
+console.info(xcounter.get())//nilai yang muncul 3 
+console.info(xcounter);//di dalam ada nilai dari #counter yaitu 3
+    
