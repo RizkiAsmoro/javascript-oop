0059d0c0cd05299d778648b365b30a0f52d24137
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-20 13:38:09 +0700

    remove

--- scripts/private-method.js
+++
@@ -1,5 +0,0 @@
-/*
-Private Method:
-- Caranya sama seperti private field dengan menambahkan # di awal method
-- Update EcmaScript dapat diakses di : https://github.com/tc39/proposal-private-methods
-*/
