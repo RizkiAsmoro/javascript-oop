6b7e21b0e57e88b3d8fe918c3abbb741185f4656
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-22 12:03:43 +0700

    operator instanceof in class inheritance

--- scripts/operator-instanceof.js
+++ scripts/operator-instanceof.js
@@ -6,22 +6,47 @@ Operator Instanceof :
 */
 
 // Cek instance rizki dan asmoro ada pada class antara teacher dan student
-class Teacher{
+
+{
+    console.info(`-------------contoh 1-----------`)
+    class Teacher{
+
+    }
+    class Student {
+    
+    }
+    
+    const rizki = new Teacher();//instance rizki
+    const asmoro = new Student();//instance asmoro
+    
+    //tidak bisa mengggunakan typeof
+    console.info(typeof rizki);// yang muncul adalah object
+    console.info(typeof asmoro);// yang muncul adalah object
+    
+    // harusnya menggunakan instanceof
+    console.info(rizki instanceof Teacher);//true
+    console.info(rizki instanceof Student);//false
+    console.info(asmoro instanceof Teacher);//false
+    console.info(asmoro instanceof Student);//true
 
 }
-class Student {
+
+{   // Operator instanceof di class inheritance
+    console.info(`-------------contoh 2-----------`)
+
+    class Employee{
+
+    }
+    class Manager extends Employee {
+
+    }
+
+    const jaka = new Employee();
+    const alex = new Manager();
+
+    console.info(jaka instanceof Employee);//true
+    console.info(jaka instanceof Manager);//false
+    console.info(alex instanceof Employee);//true, karena alex ada di class manager, class manager adalah turunan dari class employee 
+    console.info(alex instanceof Manager);//true
 
 }
-
-const rizki = new Teacher();//instance rizki
-const asmoro = new Student();//instance asmoro
-
-//tidak bisa mengggunakan typeof
-console.info(typeof rizki);// yang muncul adalah object
-console.info(typeof asmoro);// yang muncul adalah object
-
-// harusnya menggunakan instanceof
-console.info(rizki instanceof Teacher);//true
-console.info(rizki instanceof Student);//false
-console.info(asmoro instanceof Teacher);//false
-console.info(asmoro instanceof Student);//true
